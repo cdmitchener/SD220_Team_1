11b65b9299120603ca324c7becaeda5e69bfb5e7
cdmitchener <87343179+cdmitchener@users.noreply.github.com>
2022-10-09 15:58:57 -0400

    Still trying to figure out backend logic

--- Zork/main.py
+++ Zork/main.py
@@ -16,27 +16,46 @@ import zork_objects as obj
 print(fun.intro())
 
 # get input to create player
+# pname = input("What is your name? ")
+# player = obj.Player(pname)
 def create_player():
     try:
-        progress = 0
         print("What is your name?")
-        input(progress)
+
+        # player = obj.Player(pname)
 
     except EOFError:
         return
 
-create_player()
+# create_player()
+
+pyscript.write("output", create_player())
+
+def input():
+    form = cgi.FieldStorage()
+    # assign the player's input to the variable input
+    # player_input=form["prompt-input"].value
+    player_input= "Test"
+    progress = 0
+    match progress:
+        case 0:
+            # pass the player's name to player_instructions()
+            fun.player_instructions(player_input)
+            # pass the player's name to the class Player
+            obj.Player(player_input)
+
+    print(player_input)
 
 # -----------------------------------------------------------------------------------------
 # chels: If I can get create_proxy() to work and my logic between the py-script tags is
 #        at least on the right track, then we shouldn't need the code that's commented out.
 # -----------------------------------------------------------------------------------------
 # let the user know whats going on
-# fun.player_instructions()
+# fun.player_instructions(pname)
 
-fun.view_instructions()
+# fun.view_instructions()
 
 # Begin the actual game play
-fun.start_game()
+# fun.start_game()
 
 # pname_proxy = create_proxy()
